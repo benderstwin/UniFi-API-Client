662ed8ccece98a10cf870a8cc3330793e10f00ba
benderstwin <bryan@bwirth.com>
2018-11-15 13:12:22 -0700

    Alpha

---
+++ index.php
@@ -0,0 +1,12 @@
+**
+ * load the class using the composer autoloader
+ */
+require_once('/var/www/html/unifi/src/config.php');
+
+/**
+ * initialize the Unifi API connection class, log in to the controller and request the alarms collection
+ * (this example assumes you have already assigned the correct values to the variables used)
+ */
+$unifi_connection = new UniFi_API\Client($controller_user, $controller_password, $controller_url, $site_id, $controller_version, true);
+$login            = $unifi_connection->login();
+$results          = $unifi_connection->list_alarms(); // returns a PHP array containing alarm objects
